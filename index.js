788d7434777cc15e18b2d9ab2f064daeb8e30268
Tom Chambers <tom.chambers@gmail.com>
2014-10-06 18:59:22 +0100

    set port for heroku

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 var http = require('http');
 var url = require('url');
 
+var port = process.env.PORT || 5000;
+
 http.createServer(function (req, res) {
 	res.writeHead(200);
 
@@ -13,7 +15,7 @@ http.createServer(function (req, res) {
 	}
 
 	res.end();
-}).listen(5000);
+}).listen(port);
 
 var distance = require('gps-distance');
 
